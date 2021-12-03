992aeac33b96cd40fe777bc2494076a11fcd1627
J-Soriano <jsoriano.tech@gmail.com>
2021-12-03 01:42:10 -0500

    Finished adding all elements required.

--- rps.js
+++ rps.js
@@ -1,93 +1,99 @@
+//Game code
 
 const rpsval = ['rock', 'paper', 'scissors'];
-let computerSelection = ''; //computerPlay()
-let playerSelection = ''; //playerPrompt()
-let gameStat = '';
-let gameFinP = 0;
-let gameFinC = 0;
-let gameCount = 0;
-
+const computerSelection = '';
+const playerSelection = '';
+let roundWin = '';
+let playerScore = 0;
+let computerScore = 0;
 
 
 function computerPlay() {
   let randval = rpsval[Math.floor(Math.random() * rpsval.length)];
-  computerSelection = randval;
+  return randval;
 }
 
-function playerPrompt() {
-  let inp1 = prompt ("What is your choice? (Rock, Paper, Scissors) \nYou may hit cancel to exit the game.");
-  if (inp1 === ''){
-    alert ('Please enter a choice. (Rock, Paper, Scissors)');
-    playerPrompt();
-  } else if (inp1 === null || inp1 == "exit"){
-    console.log("You have left the game.");
-    //let gameStat = "end"; Why can't I assign value directly to gamestat?
-    let arp1 = "end"
-    gameStat = arp1;
-    return false;
-  } else {
-    inp1 = inp1.toLowerCase();
-    if (inp1 == "rock" || inp1 == "paper" || inp1 == "scissors"){
-        console.log("              \nStart Round: ");
-        console.log("You have chosen " + inp1 + ".");
-        console.log("The computer has chosen " + computerSelection + ".");
-        playerSelection = inp1;
-      } else{
-          console.log("You did not choose rock, paper, or scissors.");
-          return false;
-        }
-  }
-  }
-//Function for one round of RPS
-function playRound(){
-  //console.log("test func for player: " + playerSelection);
-  //console.log("test func for computer: " + computerSelection);
-  computerPlay();
-  if (playerPrompt() != false){
-    if (playerSelection == computerSelection){
-        console.log("We have a tie!");
-        gameCount++;
-        } else if (playerSelection=="rock" && computerSelection=="scissors" || playerSelection=="paper" && computerSelection=="rock" || playerSelection=="scissors" && computerSelection=="paper"){
-          console.log("You have won this round!");
-          gameFinP++;
-          gameCount++;
-        } else {
-          console.log("You have lost this round!");
-          gameFinC++;
-          gameCount++;
-        }
-      }
-  }
 
-
-//Function to play a game of Rock, paper, scissors
-function playGame(){
-  for (i = 1; i < 20; i++) {
-    if (gameStat == "end"){
-    break;
-
-  } else if (gameFinP == 3){
-    console.log("You have won the game!");
-    break;
-  } else if (gameFinC == 3){
-    console.log("You have lost the game!");
-    break;
-  } else if (gameCount == 5 && gameFinP > gameFinC){
-    console.log("You have won the game!");
-    break;
-  } else if (gameCount == 5 && gameFinC > gameFinP){
-    console.log("You have lost the game!");
-    break;
-  } else if (gameCount == 5 && gameFinP == gameFinC){
-    console.log("The game is a Tie!");
-    break;
-  } else {
-    playRound();
-    //console.log("This is games won by computer: " +gameFinC);
-    //console.log("This is games won by you: " + gameFinP);
-    //console.log("This is total amount of games: " + gameCount);
+function playRound(playerSelection, computerSelection){
+  if (playerSelection == computerSelection){
+    roundWin = "tie";
   }
+  else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection =="rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ){
+    playerScore++
+    roundWin = "player";
+  }
+  else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection =="rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+    computerScore++
+    roundWin = 'computer';
   }
 }
 
-playGame();
+function updateScore(){
+  if(roundWin == 'tie'){
+    rpsMsg.textContent = "It is a tie!";
+  } else if (roundWin == 'player'){
+    rpsMsg.textContent = "You won!";
+  } else if (roundWin == 'computer') {
+    rpsMsg.textContent = "You lost!";
+  }
+  playScore.textContent = playerScore;
+  compScore.textContent = computerScore;
+}
+
+function gameOver() {
+  if(playerScore == 5 || computerScore == 5){
+      return true;
+    }
+  }
+
+  function checkWinner(){
+    if (playerScore == 5) {
+      rpsMsg.textContent = "You have won the game! Select Rock, Paper, or Scissors to restart game.";
+    } else if (computerScore == 5) {
+      rpsMsg.textContent = "You have lost the game! Select Rock, Paper, or Scissors to restart game.";
+    }
+  }
+
+  function gameRestart(){
+  playerScore = 0
+  computerScore = 0
+  rpsMsg.innerHTML = 'Welcome to a game of rock, paper, scissors! <br>Please select one of the options below to begin.';
+  playScore.textContent = 0;
+  compScore.textContent = 0;
+  }
+
+//Links to UI
+
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const rpsMsg = document.getElementById('rpsMessage');
+const compScore = document.getElementById('computerScore');
+const playScore = document.getElementById('playerScore');
+
+rockBtn.addEventListener("click", () => clickStart("rock"));
+paperBtn.addEventListener("click", () => clickStart("paper"));
+scissorsBtn.addEventListener("click", () => clickStart("scissors"));
+
+
+function clickStart(playerSelection) {
+  if(gameOver()){
+    gameRestart();
+    return
+  }
+  const computerSelection = computerPlay();
+  console.log("You have chosen: " + playerSelection);
+  console.log("The computer has chosen: " + computerSelection);
+  playRound(playerSelection, computerSelection);
+  updateScore();
+  checkWinner();
+
+}
